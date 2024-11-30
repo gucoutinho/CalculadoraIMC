465ac965c889ef6089a9a4294516948553505fed
Calculadora Imc <gustavocoutinho5693@gmail.com>
2024-11-29 22:06:25 -0300

    calculando o imc

--- app/src/main/java/com/gustavocoutinho/primeiroapplicativo/MainActivity.kt
+++ app/src/main/java/com/gustavocoutinho/primeiroapplicativo/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
             val alturaQ2 = altura * altura
             val resultado = peso / alturaQ2
-            println("Gustavo ação do Botao = "+ resultado)
+            println("Gustavo ação do Botao = " + resultado)
         }
     }
 }
