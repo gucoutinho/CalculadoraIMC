d82ecc01f3584c8230eb5f28131a4d3912ce2cce
Calculadora Imc <gustavocoutinho5693@gmail.com>
2024-11-29 22:08:10 -0300

    calculando o imc

--- app/src/main/java/com/gustavocoutinho/primeiroapplicativo/MainActivity.kt
+++ app/src/main/java/com/gustavocoutinho/primeiroapplicativo/MainActivity.kt
@@ -2,11 +2,7 @@ package com.gustavocoutinho.primeiroapplicativo
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
